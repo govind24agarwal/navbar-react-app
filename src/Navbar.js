9b2bfd212f625025b21c0b5f02887c74e09e0e85
Govind <govindagarwal24@gmail.com>
2021-05-05 15:01:57 +0530

    navbar completed for mobile devices

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+import { AiFillCloud } from "react-icons/ai";
+import { FaBars } from "react-icons/fa";
+import data from "./data";
 
 function Navbar() {
-  return <nav>Navbar</nav>;
+  const refLinks = useRef(null);
+  const refLinksContainer = useRef(null);
+  const [showLinks, setShowLinks] = useState(false);
+
+  useEffect(() => {
+    const linkHeight = refLinks.current.clientHeight;
+    if (showLinks) {
+      refLinksContainer.current.style.height = `${linkHeight + 16}px`;
+    } else {
+      refLinksContainer.current.style.height = `0px`;
+    }
+    console.log(refLinksContainer.current.clientHeight);
+  }, [showLinks]);
+
+  return (
+    <nav>
+      <div className="header">
+        <button className="logo">
+          <AiFillCloud />
+          cloud services
+        </button>
+        <button onClick={() => setShowLinks((oldState) => !oldState)}>
+          <FaBars />
+        </button>
+      </div>
+      <div className="links-container" ref={refLinksContainer}>
+        <ul className="links" ref={refLinks}>
+          {data.map((singleLink) => {
+            const { id, name, link } = singleLink;
+            return (
+              <li key={id}>
+                <a href={link}>{name}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
